dd2864de10292fe8c6e99215169e1c384c8c7c7f
kimd0 <38683738+kimd0@users.noreply.github.com>
2022-12-07 12:56:04 +0900

    Update Agent.cs

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -10,6 +10,7 @@ public class Agent : MonoBehaviour
     Shop shop;
     double timer;
     int waitingTime;
+    int[,] evaluated_board;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,8 @@ public class Agent : MonoBehaviour
         buildManager = BuildManager.instance;
         shop = Shop.instance;
         timer = 0.0;
-        waitingTime = 5;
+        waitingTime = 1;
+        evaluated_board = new int[16, 16];
         //GetBoard();
     }
 
@@ -29,24 +31,98 @@ public class Agent : MonoBehaviour
         {
             
             List<Node> node_list = GetNodes();
-            //int[,] current_board = GetBoard();
             
             if (PlayerStats.Money >= 100)
             {
                 //build turret at (random) node
                 //노드를 선택해야 함
-                RandomBuild(node_list);
+                //BuildTower(RandomNode(node_list));
+                BuildTower(BestNode());
             }
-            GetBoard();
             
-            Debug.Log($"Agent running\nNode List Count: {node_list.Count}");
+            Debug.Log($"Agent running!");
             timer = 0;
         }
     }
 
-    public void RandomBuild(List<Node> node_list)
+    public Node RandomNode(List<Node> node_list)
     {
-        BuildTower(node_list[Random.Range(0, node_list.Count)]);
+        return node_list[Random.Range(0, node_list.Count)];
+    }
+
+    public Node BestNode()
+    {
+        int best_score = 0;
+        Point best_point = new Point(-1, -1);
+        int[,] current_board = GetBoard();
+
+        for (int i=0; i < 16; i++)
+        {
+            for (int j=0; j<16; j++)
+            {
+                if (current_board[i, j] == 0 || evaluated_board[i, j] == -1)
+                    continue;
+                int temp = 0;
+                if ( i-1 > -1 && j-1 > -1 && current_board[i-1, j-1] == 0)
+                    temp++;
+                if ( i-1 > -1 && current_board[i-1, j] == 0)
+                    temp++;
+                if ( i-1 > -1 && j+1 < 16 && current_board[i-1, j+1] == 0)
+                    temp++;
+
+                if ( j-1 > -1 && current_board[i, j-1] == 0)
+                    temp++;
+                if ( j+1 < 16 && current_board[i, j+1] == 0)
+                    temp++;
+
+                if ( i+1 < 16 && j-1 > -1 && current_board[i+1, j-1] == 0)
+                    temp++;
+                if ( i+1 < 16 && current_board[i+1, j] == 0)
+                    temp++;
+                if ( i+1 < 16 && j+1 < 16 && current_board[i+1, j+1] == 0)
+                    temp++;
+                evaluated_board[i, j] = temp;
+            }
+        }
+
+        for (int i=0; i < 16; i++)
+        {
+            for (int j=0; j<16; j++)
+            {
+                if (best_score < evaluated_board[i, j])
+                {
+                    best_score = evaluated_board[i, j];
+                    best_point.x = i;
+                    best_point.y = j;
+                }
+            }
+        }
+        // 빌드할 곳을 찾은 경우 반환
+        if (best_point.x != -1)
+            return PointToNode(best_point);
+        
+        // 그렇지 못한 경우 한칸 넓혀서 탐색
+        /*
+            구현필요
+        */
+        // 다시 null인 경우 업그레이드 시도?
+
+        for (int i=0; i < 16; i++)
+        {
+            for (int j=0; j<16; j++)
+            {
+                if (evaluated_board[i, j] == -1)
+                {
+                    //업그레이드 (i,j) -> 60원이니까 호출됐을 경우 실행이 가능하긴함... 따로 빼낼 것.
+                    best_point.x = i;
+                    best_point.y = j;
+                    PointToNode(best_point).UpgradeTurret();
+                    evaluated_board[i, j] = -2;
+                    return null;
+                }
+            }
+        }
+        return null;
     }
 
     public List<Node> GetNodes()
@@ -63,7 +139,7 @@ public class Agent : MonoBehaviour
         return node_list;
     }
 
-    struct Point
+    public struct Point
     {
         public int x, y;
         public Point(int _x, int _y)
@@ -81,9 +157,10 @@ public class Agent : MonoBehaviour
         foreach(Node node in node_list)
         {
             //Vector3 vec = node.GetBuildPosition();
-            Vector3 vec = node.transform.position;
-            Debug.Log($"{vec.x}, {vec.y}, {vec.z}");
-            current_board[(int)(vec.x/5), (int)((-vec.z)/5)] = 1;
+            //Vector3 vec = node.transform.position;
+            Point point = NodeToPoint(node);
+            current_board[point.x, point.y] = 1;
+            Debug.Log($"{point.x}, {point.y} = {current_board[point.x, point.y]}");
         }
         /*
         // 보드 확인용 출력
@@ -102,27 +179,40 @@ public class Agent : MonoBehaviour
         */
         return current_board;
     } 
-/*
+
     //노드는 가로 x=0~75, 세로 z=0~-75까지 존재함.
     // 노드를 보드형태의 좌표로 반환
     public Point NodeToPoint(Node _node)
     {
         Point result_point;
+        Vector3 node_vector = _node.transform.position;
+        result_point.x = (int)(node_vector.x/5);
+        result_point.y = (int)((-node_vector.z)/5);
         
         return result_point;
     }
 
     // 보드의 좌표를 노드로 반환
-    public Node PointToNode(int x, int y)
+    public Node PointToNode(Point _point)
     {
-        Node result_node;
-        
-        return result_node;
+        Vector3 node_vector = new Vector3(_point.x * 5, 0, _point.y * -5);
+        List<Node> node_list = GetNodes();
+        foreach(Node node in node_list)
+        {
+            Vector3 temp_vector = node.transform.position;
+            if (node_vector == temp_vector)
+            {
+                return node;
+            }
+        }
+        return null;
     }
-*/
+
     // 전달받은 노드에 특정 타워를 빌드함. 타입 없을 시 기본 타워.
     public void BuildTower(Node _node, int type = 1)
     {
+        if (_node == null)
+            return;
         if (_node.turret != null)
 			return;
         switch (type)
@@ -137,5 +227,8 @@ public class Agent : MonoBehaviour
                 _node.BuildTurret(shop.laserBeamer);
                 break;
         }
+        Point builtat = NodeToPoint(_node);
+        Debug.Log($"Build and Set Score -1 to point( {builtat.x}, {builtat.y} )");
+        evaluated_board[builtat.x, builtat.y] = -1;
     }
 }
