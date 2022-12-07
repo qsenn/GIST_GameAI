a714a070198a82578244d954f9499191219a7c87
kimd0 <38683738+kimd0@users.noreply.github.com>
2022-12-07 13:41:56 +0900

    Fix Agent tower upgrade

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -9,7 +9,7 @@ public class Agent : MonoBehaviour
     BuildManager buildManager;
     Shop shop;
     double timer;
-    int waitingTime;
+    double waitingTime;
     int[,] evaluated_board;
 
     // Start is called before the first frame update
@@ -18,7 +18,7 @@ public class Agent : MonoBehaviour
         buildManager = BuildManager.instance;
         shop = Shop.instance;
         timer = 0.0;
-        waitingTime = 1;
+        waitingTime = 0.5;
         evaluated_board = new int[16, 16];
         //GetBoard();
     }
@@ -42,7 +42,9 @@ public class Agent : MonoBehaviour
             
             Debug.Log($"Agent running!");
             timer = 0;
+            //PlayerStats.Money += 100;
         }
+        
     }
 
     public Node RandomNode(List<Node> node_list)
@@ -60,7 +62,7 @@ public class Agent : MonoBehaviour
         {
             for (int j=0; j<16; j++)
             {
-                if (current_board[i, j] == 0 || evaluated_board[i, j] == -1)
+                if (current_board[i, j] == 0 || evaluated_board[i, j] < 0)
                     continue;
                 int temp = 0;
                 if ( i-1 > -1 && j-1 > -1 && current_board[i-1, j-1] == 0)
@@ -85,6 +87,7 @@ public class Agent : MonoBehaviour
             }
         }
 
+        //PrintBoard(evaluated_board);
         for (int i=0; i < 16; i++)
         {
             for (int j=0; j<16; j++)
@@ -107,7 +110,7 @@ public class Agent : MonoBehaviour
         */
         // 다시 null인 경우 업그레이드 시도?
 
-        for (int i=0; i < 16; i++)
+        for (int i=0; i<16; i++)
         {
             for (int j=0; j<16; j++)
             {
@@ -116,8 +119,10 @@ public class Agent : MonoBehaviour
                     //업그레이드 (i,j) -> 60원이니까 호출됐을 경우 실행이 가능하긴함... 따로 빼낼 것.
                     best_point.x = i;
                     best_point.y = j;
-                    PointToNode(best_point).UpgradeTurret();
+                    
                     evaluated_board[i, j] = -2;
+                    PointToNode(best_point).UpgradeTurret();
+                    Debug.Log($"Upgrade and Set Score -2 to point( {best_point.x}, {best_point.y} )");
                     return null;
                 }
             }
@@ -160,25 +165,31 @@ public class Agent : MonoBehaviour
             //Vector3 vec = node.transform.position;
             Point point = NodeToPoint(node);
             current_board[point.x, point.y] = 1;
-            Debug.Log($"{point.x}, {point.y} = {current_board[point.x, point.y]}");
+            //Debug.Log($"{point.x}, {point.y} = {current_board[point.x, point.y]}");
         }
         /*
         // 보드 확인용 출력
-        Debug.Log($"--------board--------------");
+        PrintBoard(current_board);
+        */
+        return current_board;
+    } 
+
+    // 보드 출력 함수
+    public void PrintBoard(int[,] board)
+    {
+        Debug.Log($"--------------board--------------");
         string temp = "";
         for (int i=0; i < 16; i++)
         {
             for (int j=0; j<16; j++)
             {
-                temp = temp + " " + current_board[j, i].ToString();
+                temp = temp + " " + board[j, i].ToString();
             }
             temp = temp + "\n";
         }
         Debug.Log($"{temp}");
-        Debug.Log($"---------------------------");
-        */
-        return current_board;
-    } 
+        Debug.Log($"---------------------------------");
+    }
 
     //노드는 가로 x=0~75, 세로 z=0~-75까지 존재함.
     // 노드를 보드형태의 좌표로 반환
@@ -230,5 +241,6 @@ public class Agent : MonoBehaviour
         Point builtat = NodeToPoint(_node);
         Debug.Log($"Build and Set Score -1 to point( {builtat.x}, {builtat.y} )");
         evaluated_board[builtat.x, builtat.y] = -1;
+        //PrintBoard(evaluated_board);
     }
 }
